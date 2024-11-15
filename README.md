c8a52c471c0bbb77572fc17875c3c7b5d3983053
Kushagra Saxena <kushagrasaxena061@gmail.com>
2024-11-15 16:36:50 +0530

    backend complete

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-START 08:58:00
-
 TODO: Import-card mein price is hardcoded change it
 TODO: SEED DATA IS AT 9:30:23
 TODO: Category stats not working
+TODO: Change Logo and name
+TODO: Change NAV buttons to this #A64D79 colour
--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -6,7 +6,7 @@ export const WelcomeMsg = () => {
       <h2 className="text-2xl lg:text-4xl text-white font-medium">
         Welcome Back 👋
       </h2>
-      <p className="text-sm lg:text-base text-indigo-500">
+      <p className="text-sm lg:text-base text-[#A64D79]">
         This is your Financial Overview Report
       </p>
     </div>
