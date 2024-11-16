e4d34161a67e0f2b3efd920f5d77c3ca9b2dbfab
Kushagra Saxena <kushagrasaxena061@gmail.com>
2024-11-16 21:54:33 +0530

    logo added

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -6,7 +5,8 @@ export const HeaderLogo = () => {
   return (
     <Link href="/">
       <div className=" items-center hidden lg:flex">
-        <Image src="/logo.jpeg" alt="Logo" height={64} width={64} />
+        {/* <Image src="/logo.jpeg" alt="Logo" height={64} width={64} /> */}
+        <div className="text-white font-semibold p-2 rounded-md mx-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">PennyWise</div>
       </div>
     </Link>
   );
