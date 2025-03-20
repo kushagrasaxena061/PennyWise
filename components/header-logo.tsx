027a0233b33385f06965dc40069b9cb5bc7a256a
Kushagra Saxena <kushagrasaxena061@gmail.com>
2025-03-20 18:37:20 +0530

    bugs fixed

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 export const HeaderLogo = () => {
   return (
     <Link href="/">
       <div className=" items-center hidden lg:flex">
-        {/* <Image src="/logo.jpeg" alt="Logo" height={64} width={64} /> */}
-        <div className="text-white font-semibold p-2 rounded-md mx-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">PennyWise</div>
+        <Image src="/Pennywise.jpeg" alt=" Main Logo" height={64} width={64} />
+        
       </div>
     </Link>
   );
